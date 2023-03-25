41e83bf3964f90c5b70a79d91dc218a78a0f3176
Great Joycs <pavigreatjoycs@gmail.com>
2023-03-25 12:45:00 +0530

    added second line to MySample

--- src/main/java/com/luv2code/pkg/MySample.java
+++ src/main/java/com/luv2code/pkg/MySample.java
@@ -3,7 +3,7 @@ public class MySample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("MY Sample code");
-		syso
+		System.out.println("Second line be greatful");
 	}
 
 }
