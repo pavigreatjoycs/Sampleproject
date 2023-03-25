f0e47f0a4fd7017b40141013507fb8ccd7e1d1e1
Great Joycs <pavigreatjoycs@gmail.com>
2023-03-25 12:44:04 +0530

    My Commit from pc

--- src/main/java/com/luv2code/pkg/MySample.java
+++ src/main/java/com/luv2code/pkg/MySample.java
@@ -1,10 +1,9 @@
-package com.luv2code.pkg;
-
 public class MySample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("MY Sample code");
+		syso
 	}
 
 }
